f187f14e5df220d5aeaf45674a2fa09bca49bb91
DESKTOP-LO6P4OE\Rintaman <mahmoud.k.elkholy@gmail.com>
2021-07-25 12:46:28 +0200

    changed url to /api

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: 'NEWSHUES BLOG',
+    API_PRODUCTION: 'http://newshuesblog.com',
+    //API_DEVELOPMENT: 'http://localhost:8000/api',
+    API_DEVELOPMENT: '/api',
+    PRODUCTION: false
+  }
+};
